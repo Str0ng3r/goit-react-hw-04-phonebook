6ab007169dc72c77a1dd230bf196e2619bcd7459
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-05-25 13:37:29 +0200

    UPDATE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,76 @@
-import {useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import { RenderForm } from "./RenderForm";
 import { ListRender } from "./ListRender";
 import { FilterRend } from "./FilterRend";
 import { Notify } from "./Notify";
+
 export const App = () => {
+  const [contact, setContact] = useState([]);
+  const [filter, setFilter] = useState("");
 
-const [contact,setContact] = useState([])
-const [filter,setFilter] = useState('')
-
-
-useEffect(()=> {
-  const localContacts = JSON.parse(localStorage.getItem('contacts'));
-  if (localContacts) {
-    setContact(localContacts);
-  }
-},[]);
-
-
-
-const filterContactsByName = () => {
-  return contact.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
-
-
-const filterFunc = (evt) => {
-setFilter(evt.target.value)
-}
-
-
-
-
-const deleteContact = (id) => {
-  const updatedContacts = contact.filter((contact) => contact.id !== id);
-  setContact(updatedContacts);
-  localStorage.setItem('contacts', JSON.stringify(updatedContacts)); // Исправлено на updatedContacts
-};
-
-
-
-
-
-const addContact = (name, number) => {
-  const isNameAlreadyExists = contact.some(
-    contact => contact.name === name
-  );
-  if (isNameAlreadyExists) {
-    alert('Таке імя вже існує');
-    return;
-  }
-
-  const newContact = {
-    id: nanoid(),
-    name:name,
-    number:number,
-  };
-setContact([...contact, newContact])
-      localStorage.setItem(
-        'contactsLocale',
-        JSON.stringify(contact)
-      );
+  useEffect(() => {
+    const localContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (localContacts) {
+      setContact(localContacts);
     }
+  }, []);
+
+  const filterContactsByName = () => {
+    return contact.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const filterFunc = (evt) => {
+    setFilter(evt.target.value);
+  };
+
+  const deleteContact = (id) => {
+    const updatedContacts = contact.filter((contact) => contact.id !== id);
+    setContact(updatedContacts);
+    localStorage.setItem("contacts", JSON.stringify(updatedContacts));
+  };
+
+  const addContact = (name, number) => {
+    const isNameAlreadyExists = contact.some((contact) => contact.name === name);
+    if (isNameAlreadyExists) {
+      alert("Таке імя вже існує");
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    setContact([...contact, newContact]);
+  };
+
+// useEffect(()=> {
+//   const localData = JSON.parse(localStorage.getItem('contacts'))
+//   setContact(localData)
+// },[])
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contact));
+  }, [contact]);
 
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         fontSize: 40,
-        color: '#010101'
+        color: "#010101",
       }}
     >
-<RenderForm addContact={addContact}></RenderForm>
-{contact.length>0 && <ListRender arrayFunc={filterContactsByName} deleteFunc={deleteContact}></ListRender>}
-{contact.length === 0 && <Notify message={'plz add new contact'}></Notify>}
-<FilterRend filtertg={filter} funcfiltr={filterFunc}></FilterRend>
+      <RenderForm addContact={addContact}></RenderForm>
+      {contact.length > 0 && <ListRender arrayFunc={filterContactsByName} deleteFunc={deleteContact}></ListRender>}
+      {contact.length === 0 && <Notify message={"plz add new contact"}></Notify>}
+      <FilterRend filtertg={filter} funcfiltr={filterFunc}></FilterRend>
     </div>
   );
 };
